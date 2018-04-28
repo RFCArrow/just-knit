d087ed5a6e59d6b0e8a305dce67471bc321c1a30
Rob Arrow <RFCArrow@gmail.com>
2018-04-28 17:28:53 +0100

    Added Development Service and first characteristic and tidied up .gitignore

--- nrf52/ble_app_template/pca10040/s132/armgcc/_build/nrf52832_xxaa/gcc_startup_nrf52.S.d
+++
@@ -1,2 +0,0 @@
-_build/nrf52832_xxaa/gcc_startup_nrf52.S.o: \
- /home/rob/nrf52/nRF5_SDK_14.2.0_17b948a/components/toolchain/gcc/gcc_startup_nrf52.S
---
+++ nrf52/ble_gatt_app/ble_devs.h
@@ -0,0 +1,69 @@
+#ifndef _BLE_DEV_SERV_H__
+#define _BLE_DEV_SERV_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "ble.h"
+#include "ble_srv_common.h"
+
+
+/**@brief   Macro for defining a ble_dev_serv instance.
+ *
+ *@param   _name   Name of the instance.
+ *@hideinitializer
+*/
+#define BLE_DEVS_DEF(_name) \
+static ble_devs_t _name;
+
+// 93e6af3d-20b6-4a6d-9250-ce8d584b70df
+#define DEV_SERVICE_UUID_BASE         {0x93, 0xE6, 0xAF, 0x3D, 0x20, 0xB6, 0x4A, 0x6D, \
+                                              0x92, 0x50, 0xCE, 0x8D, 0x58, 0x4B, 0x70, 0xDF}
+#define DEV_SERVICE_UUID    0x1400
+#define GYRO_CHAR_UUID      0x1401
+#define ACCEL_CHAR_UUID     0x1402
+#define QUART_CHAR_UUID     0x1403
+#define PEDAL_CHAR_UUID     0x1404
+
+
+/**@brief Custom Service init structure. This contains all options and data needed for
+ *        initialization of the service.*/
+typedef struct
+{
+        uint8_t                       initial_dev_value;           /**< Initial custom value */
+        ble_srv_cccd_security_mode_t  value_char_attr_md;     /**< Initial security level for Custom characteristics attribute */
+} ble_devs_init_t;
+
+
+/**@brief Custom Service structure. This contains various status information for the service. */
+struct ble_devs_s
+{
+        uint16_t                        service_handle;             /**< Handle of Custom Service (as provided by the BLE stack). */
+        ble_gatts_char_handles_t        value_handles;           /**< Handles related to the Custom Value characteristic. */
+        uint16_t                        conn_handle;                    /**< Handle of the current connection (as provided by the BLE stack, is BLE_CONN_HANDLE_INVALID if not in a connection). */
+        uint8_t                       uuid_type; 
+};
+
+// Forward declaration of the ble_devs_t type.
+typedef struct ble_devs_s ble_devs_t;
+
+/**@brief Function for initializing the Custom Service.
+ *
+ * @param[out]  p_devs       Custom Service structure. This structure will have to be supplied by
+ *                          the application. It will be initialized by this function, and will later
+ *                          be used to identify this particular service instance.
+ * @param[in]   p_devs_init  Information needed to initialize the service.
+ *
+ * @return      NRF_SUCCESS on successful initialization of service, otherwise an error code.
+ */
+uint32_t ble_devs_init(ble_devs_t * p_devs, const ble_devs_init_t * p_devs_init);
+
+/**@brief Function for adding the Custom Value characteristic.
+ *
+ * @param[in]   p_cus        Custom Service structure.
+ * @param[in]   p_cus_init   Information needed to initialize the service.
+ * @return      NRF_SUCCESS on success, otherwise an error code.
+ */
+//static uint32_t devs_value_char_add(ble_devs_t * p_devs, const ble_devs_init_t * p_devs_init);
+
+
+#endif
