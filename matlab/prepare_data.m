fa73891b7bce1fd3b6e37c6520aaeec6b4745b80
Rob Arrow <RFCArrow@gmail.com>
2018-05-26 18:24:39 +0100

    Started work on matlab script to prepare new data

---
+++ matlab/prepare_data.m
@@ -0,0 +1,49 @@
+function out_data = prepare_data(folder_name)
+
+root_dir = "/home/rob/just-knit/csv/"
+
+cd(char(strcat(root_dir,folder_name)))
+
+files = strsplit(ls)
+
+for i = 1:(length(files)-1)
+   table = readtable(files{i})
+   table=table(~any(ismissing(table(1:16)),2),:);
+   
+   
+end
+
+for i = 1:size(data,1)-1
+    if( data{i,16} ~= data{i+1,16} )
+        if(exist('gesture'))
+            gesture{end+1}(1,:) = data{i,:};
+        else
+            gesture{1}(1,:) = data{i,:};
+        end
+    else 
+        if(exist('gesture'))
+            gesture{end}(end+1,:) = data{i,:};
+        else
+            gesture{1}(1,:) = data{i,:};
+        end
+    end
+end
+
+
+for i = 1:size(gesture,2)
+   if(gesture{i}(1,16) == 0)
+       if(exist('knit')) 
+           knit{end+1} = gesture{i};
+       else
+           knit{1} = gesture{i};
+       end
+   else
+       if(exist('no_knit'))
+           no_knit{end+1} = gesture{i};
+       else
+           no_knit{1} = gesture{i};
+       end
+   end
+end
+   
+    
