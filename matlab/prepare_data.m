a2d65323d2948944e77a44c380de2e455e65d3fe
RFCArrow <RFCASpam@gmail.com>
2018-05-27 23:02:29 +0100

    MATLAB segments data in cell array now

--- matlab/prepare_data.m
+++ matlab/prepare_data.m
@@ -1,49 +1,56 @@
-function out_data = prepare_data(folder_name)
+function out_data = prepare_data(folder_name, stitch, direction, knitter)
 
-root_dir = "/home/rob/just-knit/csv/"
+prev_dir = pwd;
 
-cd(char(strcat(root_dir,folder_name)))
-
-files = strsplit(ls)
-
-for i = 1:(length(files)-1)
-   table = readtable(files{i})
-   table=table(~any(ismissing(table(1:16)),2),:);
-   
-   
+if ispc
+    root_dir = 'C:\Users\Rob\Documents\just-knit\csv\';
+    cd(char(strcat(root_dir,folder_name,'\')))
+    list = ls;
+    files = string(list(3:end,:));
+elseif isunix
+    root_dir = "/home/rob/just-knit/csv/"  ;
+    cd(char(strcat(root_dir,folder_name,'/')));
+    files=strsplit(ls);
+    files = files(1:end-1);
 end
 
-for i = 1:size(data,1)-1
-    if( data{i,16} ~= data{i+1,16} )
-        if(exist('gesture'))
-            gesture{end+1}(1,:) = data{i,:};
-        else
-            gesture{1}(1,:) = data{i,:};
-        end
-    else 
-        if(exist('gesture'))
-            gesture{end}(end+1,:) = data{i,:};
-        else
-            gesture{1}(1,:) = data{i,:};
-        end
+for i = 1:(length(files))
+    table = readtable(files{i},'ReadVariableNames',false,'Format','%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f'); 
+    table = rmmissing(table(:,1:16),1);
+    array = table2array(table(:,1:16));
+    
+    transitions = find( diff(array(:,16)==1) | diff(array(:,16)==-1) );
+    transitions = transitions+1;
+    
+    if transitions(1) ~= 1
+        transitions = [1; transitions];
+    end
+    
+    if transitions(end) ~= size(array,1)+1
+        transitions = [transitions; size(array,1)+1];
+    end
+    
+    Cell = mat2cell(array,diff(transitions),16);
+    
+    if i == 1
+        data = Cell;
+    else
+        data = [data; Cell];
     end
 end
+    
+
+isNotStitch = find(cellfun( @(x) x(1,16), data(:,1) ) == 0);
+isStitch =  find(cellfun( @(x) x(1,16), data(:,1) ) == 1);
 
 
-for i = 1:size(gesture,2)
-   if(gesture{i}(1,16) == 0)
-       if(exist('knit')) 
-           knit{end+1} = gesture{i};
-       else
-           knit{1} = gesture{i};
-       end
-   else
-       if(exist('no_knit'))
-           no_knit{end+1} = gesture{i};
-       else
-           no_knit{1} = gesture{i};
-       end
-   end
-end
+data(isNotStitch,2) = cellstr('none');
+data(isStitch,2) = cellstr(stitch);
+
+data(:,3) = cellstr(direction);
+data(:,4) = cellstr(knitter);
    
+cd(prev_dir);
+out_data = data;
+end
     
