d8e742389d5af0be50d0a96e0802494781497237
RFCArrow <RFCASpam@gmail.com>
2018-06-02 17:37:57 +0100

    Added diff data

--- matlab/workpad.m
+++ matlab/workpad.m
@@ -6,8 +6,11 @@ data_5 = prepare_data('purl_to_main','purl','to','rob');
 
 data_set = [data_1; data_2; data_3; data_4; data_5];
 
+diff_data = data_set;
+diff_data(:,1) = cellfun( @diff, data_set(:,1), 'UniformOutput', false) 
+
 %Initial Test
-test_dtw_cell(data_set,[10:10:200], 10, [1 2 3 4]);
+%test_dtw_cell(data_set,[10:10:200], 10, [1 2 3 4]);
 
 %Add weighting
 %Weighting can't be inverse of sorted index because the closest match will
@@ -27,6 +30,7 @@ test_dtw_cell(data_set,[10:10:200], 10, [1 2 3 4]);
 
 
 %Extract rate of change of quarternion?
+test_dtw_cell(diff_data,[10:10:200], 10, 1);
 
 
 %Try justs acc
