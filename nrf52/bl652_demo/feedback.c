ac6d1d738a33b0278a0efa2f7c712bcc5d7dbe71
Rob Arrow <RFCArrow@gmail.com>
2018-06-09 16:55:57 +0100

    Included LEDs in feedback system

--- nrf52/bl652_demo/feedback.c
+++ nrf52/bl652_demo/feedback.c
@@ -9,4 +9,15 @@ void feedback_init(){
     nrf_gpio_cfg_output(BUZZ_PIN);
     nrf_gpio_pin_clear(BUZZ_PIN);
 
+    
+    nrf_gpio_cfg_output(RED_PIN);
+    nrf_gpio_pin_clear(RED_PIN);
+
+    
+    nrf_gpio_cfg_output(BLUE_PIN);
+    nrf_gpio_pin_clear(BLUE_PIN);
+
+
+    nrf_gpio_cfg_output(GREEN_PIN);
+    nrf_gpio_pin_clear(GREEN_PIN);
 }
--- nrf52/bl652_demo/feedback.h
+++ nrf52/bl652_demo/feedback.h
@@ -3,9 +3,13 @@
 
 #include "nrf_gpio.h"
 
+#define RED_PIN             NRF_GPIO_PIN_MAP(0,8)
+#define BLUE_PIN            NRF_GPIO_PIN_MAP(0,7)
+#define GREEN_PIN           NRF_GPIO_PIN_MAP(0,6)
 
 #define MOTOR_PIN           NRF_GPIO_PIN_MAP(0,3)
 #define BUZZ_PIN            NRF_GPIO_PIN_MAP(0,2)
+
 
 
 
