c0669c3cf569e8e56f06e15fffe0ef2cc4f3ae81
Rob Arrow <RFCArrow@gmail.com>
2018-06-10 00:09:45 +0100

    Began creating feedback FSM (incomplete)

--- nrf52/bl652_demo/feedback.c
+++ nrf52/bl652_demo/feedback.c
@@ -9,15 +9,82 @@ void feedback_init(){
     nrf_gpio_cfg_output(BUZZ_PIN);
     nrf_gpio_pin_clear(BUZZ_PIN);
 
-    
+   //Initialise LEDs 
     nrf_gpio_cfg_output(RED_PIN);
     nrf_gpio_pin_clear(RED_PIN);
-
-    
     nrf_gpio_cfg_output(BLUE_PIN);
     nrf_gpio_pin_clear(BLUE_PIN);
-
-
     nrf_gpio_cfg_output(GREEN_PIN);
     nrf_gpio_pin_clear(GREEN_PIN);
+
+    //Initial feedback fsm
+    feedback_handler = fb_state_init;
 }
+
+//Initial State
+fb_state_t fb_state_init(const fb_event_t event){
+    ret_code_t err_code = NRF_SUCCESS;
+    switch(event){
+        case FB_EVT_MOVEMENT_DETECTED:
+            fb_transition(&fb_state_advertising);
+        break;
+        case default:
+            //do nothing
+        break;
+    }
+    return err_code;
+}
+
+fb_state_t fb_state_sleeping(const fb_event_t event){
+    ret_code_t err_code = NRF_SUCCESS;
+    switch(event){
+        case FB_EVT_ENTRY:
+            //turn off all lights
+        break;
+        case FB_EVT_EXIT:
+            //no need to do anything
+        break;
+        case FB_EVT_MOVEMENT_DETECTED:
+            fb_transition(&fb_state_advertising);
+        break;
+        case FB_EVT_CHARGING_BEGIN:
+            fb_transition(&fb_state_charging);
+        break;
+        default:
+            //no need to do anything
+        break;
+    }
+    return err_code;
+}
+
+fb_state_t fb_state_advertising(const fb_event_t event){
+    ret_code_t err_code = NRF_SUCCESS;
+    switch(event){
+        case FB_EVT_ENTRY:
+            //begin softblink
+        break;
+        case FB_EVT_EXIT:
+            //stop softblink
+        break;
+        case FB_EVT_BLE_CONNECT:
+            fb_transition(&fb_state_connected);
+        break;
+        case FB_EVT_ADVERTISING_TIMEOUT:
+            fb_transition(&fb_state_sleep);
+        break;
+        case FB_EVT_CHARGING_BEGIN:
+            fb_transition(&fb_state_charging);
+        break;
+        default:
+            //do nothing
+        break;
+    }
+    return err_code;
+}
+uint32_t fb_state_connected(const fb_event_t event);
+uint32_t fb_state_knit(const fb_event_t event);
+uint32_t fb_state_purl(const fb_event_t event);
+uint32_t fb_state_correct(const fb_event_t event);
+uint32_t fb_state_incorrect(const fb_event_t event);
+uint32_t fb_state_charging(const fb_event_t event);
+uint32_t fb_state_charged(const fb_event_t event);
--- nrf52/bl652_demo/feedback.h
+++ nrf52/bl652_demo/feedback.h
@@ -11,6 +11,43 @@
 #define BUZZ_PIN            NRF_GPIO_PIN_MAP(0,2)
 
 
+typedef enum{
+    FB_EVT_INIT,
+    FB_EVT_ENTRY,
+    FB_EVT_EXIT,
+    FB_EVT_MOVEMENT_TIMEOUT,
+    FB_EVT_CHARGER_DISCONNECT,
+    FB_EVT_CHARGING_BEGIN,
+    FB_EVT_CHARGING_COMPLETE,
+    FB_EVT_MOVEMENT_DETECTED,
+    FB_EVT_BLE_CONNECT,
+    FB_EVT_BLE_DISCONNECT,
+    FB_EVT_KNIT_REQUEST,
+    FB_EVT_PURL_REQUEST,
+    FB_EVT_CORRECT_STITCH,
+    FB_EVT_INCORRECT_STITCH,
+} fb_event_t;
+
+uint32_t (* feedback_handler)(const fb_event_t event);
+
+typedef uint32_t fb_state_t;
+inline void fb_transition( fb_state_t (*new_state)(const fb_event_t event) ){
+    feedback_handler(FB_EVT_EXIT);
+    feedback_handler = new_state;
+    feedback_handler(FB_EVT_ENTRY);
+}
+
+fb_state_t fb_state_init(const fb_event_t event);
+fb_state_t fb_state_sleeping(const fb_event_t event);
+fb_state_t fb_state_advertising(const fb_event_t event);
+fb_state_t fb_state_connected(const fb_event_t event);
+fb_state_t fb_state_knit(const fb_event_t event);
+fb_state_t fb_state_purl(const fb_event_t event);
+fb_state_t fb_state_correct(const fb_event_t event);
+fb_state_t fb_state_incorrect(const fb_event_t event);
+fb_state_t fb_state_charging(const fb_event_t event);
+fb_state_t fb_state_charged(const fb_event_t event);
+
 
 
 void feedback_init(void);
